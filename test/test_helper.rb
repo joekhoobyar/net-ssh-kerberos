86374131c09ad1d89a1642d8999d43dbd8e7d7b4
Joe Khoobyar <joe@ankhcraft.com>
2009-04-30 23:30:18 -0400

    Initial commit to net-ssh-kerberos.

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'net_ssh_kerberos'
+
+class Test::Unit::TestCase
+end
