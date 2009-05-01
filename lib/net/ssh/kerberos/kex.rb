9288bc1722af255c2e2c6f41550bc3fe185f4bce
Joe Khoobyar <joe@ankhcraft.com>
2009-05-01 00:49:32 -0400

    adding baseline kex

--- lib/net/ssh/kerberos/kex.rb
+++ lib/net/ssh/kerberos/kex.rb
@@ -3,12 +3,13 @@ require 'net/ssh/kerberos/kex/krb5_diffie_hellman_group1_sha1'
 module Net; module SSH; module Kerberos
   module Kex
     
-    MAP = {
+    GSS_MAP = {
       'gss-group1-sha1-toWM5Slw5Ew8Mqkay+al2g==' => KRB5DiffieHellmanGroup1SHA1,
       'gss-gex-sha1-toWM5Slw5Ew8Mqkay+al2g==' => KRB5DiffieHellmanGroupExchangeSHA1
     }
-    
-    Net::SSH::Transport::Kex::MAP.update MAP
+
+    Net::SSH::Transport::Kex::MAP.update GSS_MAP
+    Net::SSH::Transport::ALGORITHMS[:kex] << GSS_MAP.keys
 
   end
 end; end; end
