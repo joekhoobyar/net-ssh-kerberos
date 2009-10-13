fe338ac335850cdff3677b808f354744d5b15835
Joe Khoobyar <joe@ankhcraft.com>
2009-10-13 13:32:19 -0400

    no loading of SSPI on non-Windows systems

--- lib/net/ssh/kerberos.rb
+++ lib/net/ssh/kerberos.rb
@@ -1,7 +1,11 @@
 require 'net/ssh'
 require 'net/ssh/kerberos/constants'
 #require 'net/ssh/kerberos/kex'
-require 'net/ssh/kerberos/sspi'
+if RUBY_PLATFORM.include?('win') && ! RUBY_PLATFORM.include?('dar'); then
+  require 'net/ssh/kerberos/sspi'
+else
+  $stderr.puts "warning: Kerberos support for non-Windows systems is not yet implemented."
+end
 require 'net/ssh/authentication/methods/gssapi_with_mic'
 
 module Net
