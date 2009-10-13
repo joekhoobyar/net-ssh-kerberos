cd3676c6b8fc149ba9a919ffb7a00f748f70dd24
Joe Khoobyar <joe@ankhcraft.com>
2009-10-13 13:24:50 -0400

    cleaned up the SSPI hierarchy a bit

--- lib/net/ssh/kerberos/sspi.rb
+++ lib/net/ssh/kerberos/sspi.rb
@@ -1,54 +1,5 @@
 require 'net/ssh/kerberos/sspi/api'
+require 'net/ssh/kerberos/sspi/gss_context'
 
-module Net; module SSH; module Kerberos
-  module SSPI
-
-    class State
-      attr_accessor :name, :realm, :valid
-      alias :valid? :valid
-    end
-
-    # Acquires credentials for SSPI (GSSAPI) authentication, and determines
-    # the credential's username and realm.
-    def sspi_acquire_credentials(state)
-      #SecPkgCredentials_Names names
-      #char *delimiter, *cp
-
-      state.valid? and return true
-
-      #sspi->from_server_token.BufferType = SECBUFFER_TOKEN | SECBUFFER_READONLY;
-
-      # Acquire credentials
-      sspi_acquire_credentials_handle
-      names = sspi_query_credentials_names
-      state.name, state.realm = *names.user_name.split('@')
-
-      #debug(("  FreeContextBuffer(%x)\n", names.sUserName));
-      #result = SSPIResult.new(API::FreeContextBuffer(names.user.to_p))
-      sspi_free_context_buffer names
-      sspi_free_credentials_handle
-
-      # Sometimes, Microsoft SSPI returns a UPN of the form "user@REALM@",
-      # (Seen under WinXP pro after ksetup to a MIT realm). Deal with that.
-      realm.chop if realm[-1] == '@'
-
-      # Initialise the request flags.
-      #sspi->request_flags = ISC_REQ_MUTUAL_AUTH | 
-      #                       ISC_REQ_INTEGRITY |
-      #                       ISC_REQ_CONFIDENTIALITY | 
-      #                       ISC_REQ_ALLOCATE_MEMORY;
-      #if (ssh->cfg.sspi_fwd_ticket)
-      #  sspi->request_flags |= ISC_REQ_DELEGATE;
-
-      if ! sspi_construct_service_name state
-        state.valid = true
-      #if (!sspi_construct_service_name(ssh, sspi)) {
-        #debug(("  FreeCredentialsHandle(%s)\n", HDL(&sspi->credentials)));
-      else
-        sspi_free_credentials_handle
-      end
-    end 
-  end
-end; end; end
-
-
+module Net; module SSH; module Kerberos; module SSPI
+end; end; end; end
---
+++ lib/net/ssh/kerberos/sspi/gss_context.rb
@@ -0,0 +1,92 @@
+require 'net/ssh/kerberos/sspi/api'
+
+module Net; module SSH; module Kerberos; module SSPI; class GSSContext
+
+  class GeneralError < StandardError; end
+
+  include Win32::SSPI
+
+  def create(user, host)
+    dispose if @credentials or @handle
+    @credentials = CredHandle.new
+    ts=TimeStamp.new
+
+    result = SSPIResult.new(API::AcquireCredentialsHandle.call(
+                              nil, "Kerberos", SECPKG_CRED_OUTBOUND, nil, nil,
+                              nil, nil, @credentials.to_p, ts.to_p
+                            ))
+    unless result.ok?
+      @credentials = nil
+      raise GeneralError, "Error acquiring credentials: #{result}"
+    end
+    
+		buff = "\0\0\0\0"
+		result = SSPIResult.new(API::QueryCredentialsAttributes.call(@credentials.to_p, SECPKG_CRED_ATTR_NAMES, buff))
+    if result.ok?
+      names = buff.to_ptr.ptr
+      begin
+        @cred_name = names.to_s.sub /^.*\\/, ''
+        @cred_krb_name = @cred_name.gsub '@', '/';
+        @server_name = Socket.gethostbyname(host)[0]
+        @server_krb_name = "host/" + @server_name
+      ensure
+        API::FreeContextBuffer.call(names)
+      end
+    end
+    cred_names = SecPkgCredentialsNames.new(names)
+  end
+  
+  def init(token=nil)
+    ctx = CtxtHandle.new
+    ts = TimeStamp.new
+    prev = @state[:handle].to_p if @state and @state[:handle]
+    req = ISC_REQ_DELEGATE | ISC_REQ_MUTUAL_AUTH | ISC_REQ_INTEGRITY
+		output = SecurityBuffer.new
+		input = SecurityBuffer.new(token) if token
+		ctxAttr = "\0" * 4
+		result = SSPIResult.new(API::InitializeSecurityContext.call(@credentials.to_p, prev, @server_krb_name,
+					                      req, 0, SECURITY_NATIVE_DREP, input ? input.to_p : nil,
+					                      0, ctx.to_p, output.to_p, ctxAttr, ts.to_p))
+    if SEC_I_COMPLETE_NEEDED == result || SEC_I_COMPLETE_AND_CONTINUE == result
+			result = SSPIResult.new(API::CompleteAuthToken.call(ctx.to_p, output.to_p))
+    end
+    unless result.ok?
+      input.token
+      raise GeneralError, "Error initializing security context: #{result} #{input.inspect}"
+    end
+    @state = { :handle => ctx, :result => result, :token => output.token, :stamp => ts }
+    if result.value == 0
+      @sizes = SecPkgSizes.new
+			result = SSPIResult.new(API::QueryContextAttributes.call(ctx.to_p, SECPKG_ATTR_SIZES, @sizes.to_p))
+			@handle = @state[:handle]
+    end
+    @state[:token]
+  end
+  
+  def established?
+    @handle && (@handle.upper.nonzero? || @handle.lower.nonzero?) && (@state.nil? || @state[:result].value.zero?)
+  end
+  
+  def get_mic(token=nil)
+    buffers = SecurityBuffer.new 2
+    buffers.set_buffer 0, SECBUFFER_DATA, token
+    buffers.set_buffer 1, SECBUFFER_TOKEN, nil, @sizes.max_signature
+    @state[:result] = SSPIResult.new(API::MakeSignature.call(@handle.to_p, 0, buffers.to_p, 0))
+    unless @state[:result].ok?
+      raise GeneralError, "Error creating the signature: #{result}"
+    end
+    return buffers.token(1).dup
+  end
+  
+  def dispose()
+    if @credentials
+      API::FreeCredentialsHandle.call(@credentials.to_p)
+      @credentials = nil
+    end
+    if @handle
+      API::DeleteSecurityContext.call(@handle.to_p)
+      @handle = nil
+    end
+  end 
+
+end; end; end; end; end
