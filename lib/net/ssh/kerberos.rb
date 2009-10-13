8b0b0fd3ea25f2fea62122070e09206134360667
Joe Khoobyar <jkhoobyar@tnsi.com>
2009-10-13 05:23:39 -0400

    core implementation

--- lib/net/ssh/kerberos.rb
+++ lib/net/ssh/kerberos.rb
@@ -2,6 +2,7 @@ require 'net/ssh'
 require 'net/ssh/kerberos/constants'
 require 'net/ssh/kerberos/kex'
 require 'net/ssh/kerberos/sspi'
+require 'net/ssh/kerberos/authentication/methods/gssapi_with_mic'
 
 module Net
   module SSH
