b348f3bfe5ccf9bbb05a80771bf7379a987d925d
Joe Khoobyar <joe@ankhcraft.com>
2009-10-13 13:25:04 -0400

    updated message for flunked test

--- test/net_ssh_kerberos_test.rb
+++ test/net_ssh_kerberos_test.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), 'test_helper.rb')
 
 class NetSshKerberosTest < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_net_ssh_integration
+    flunk "write some unit tests for Net::SSH integration"
   end
 end
