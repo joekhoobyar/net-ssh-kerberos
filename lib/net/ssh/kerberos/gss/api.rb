c6d64785ccbdece66d6467b69e8e7b5263d8883f
Joe Khoobyar <joe@ankhcraft.com>
2009-10-14 01:09:25 -0400

    more GSSAPI implementation

--- lib/net/ssh/kerberos/gss/api.rb
+++ lib/net/ssh/kerberos/gss/api.rb
@@ -1,12 +1,17 @@
 require 'dl/import'
 require 'dl/struct'
 
-module Net; module SSH; module Kerberos; module GSS; module API
+module Net; module SSH; module Kerberos; module GSS;
 
-  extend DL::Importable
+  module API
 
-  if RUBY_PLATFORM =~ /cygwin/
-    dlload('cyggss-1.dll')
+    extend DL::Importable
+
+    if RUBY_PLATFORM =~ /cygwin/
+      dlload('cyggss-1.dll')
+    else
+      dlload('/usr/share/centrifydc/kerberos/lib/libgssapi_krb5.so')
+    end 
 
     typealias 'OM_uint32', 'unsigned int'
     typealias 'size_t', 'unsigned int'
@@ -17,10 +22,16 @@ module Net; module SSH; module Kerberos; module GSS; module API
     GssOID = struct [ "OM_uint32 length", "char *elements" ]
     typealias 'gss_OID_desc', 'GssOID'
     typealias 'gss_OID', 'gss_OID_desc *'
-    GssOIDSet = struct [ "size_t count", "GssOID *elements" ]
+    GssOIDSet = struct [ "size_t count", "gss_OID elements" ]
     typealias 'gss_OID_set_desc', 'GssOIDSet'
-    typealias 'gss_OID_set', 'gss_OID_set *'
+    typealias 'gss_OID_set', 'gss_OID_set_desc *'
 
+    #typealias 'gss_ctx_id_t', 'void *'
+    #typealias 'gss_cred_id_t', 'void *'
+    #typealias 'gss_name_t', 'void *'
+    #GssCtxId = struct [ "char *handle" ]
+    #GssCredId = struct [ "char *handle" ]
+    #GssName = struct [ "char *handle" ]
     typealias 'gss_ctx_id_t', 'void *'
     typealias 'gss_cred_id_t', 'void *'
     typealias 'gss_name_t', 'void *'
@@ -28,25 +39,32 @@ module Net; module SSH; module Kerberos; module GSS; module API
     typealias 'gss_qop_t', 'OM_uint32'
     typealias 'gss_cred_usage_t', 'int'
 
-    GSS_C_INITIATE = 1
-
-    GSS_C_DELEG_FLAG      = 1
-    GSS_C_MUTUAL_FLAG     = 2
-    GSS_C_REPLAY_FLAG     = 4
-    GSS_C_SEQUENCE_FLAG   = 8
-    GSS_C_CONF_FLAG       = 16
-    GSS_C_INTEG_FLAG      = 32
-    GSS_C_ANON_FLAG       = 64
-    GSS_C_PROT_READY_FLAG = 128
-    GSS_C_TRANS_FLAG      = 256
-
     extern "OM_uint32 gss_acquire_cred (OM_uint32 *, gss_name_t, OM_uint32, gss_OID_set, gss_cred_usage_t, gss_cred_id_t *, gss_OID_set *, OM_uint32 *)"
     extern "OM_uint32 gss_release_cred (OM_uint32 *, gss_cred_id_t *)"
+    extern "OM_uint32 gss_import_name (OM_uint32 *, gss_buffer_t, gss_OID, gss_name_t *)"
+    extern "OM_uint32 gss_init_sec_context (OM_uint32 *, gss_cred_id_t, gss_ctx_id_t *, gss_name_t, gss_OID, OM_uint32, OM_uint32, void *, gss_buffer_t, gss_OID *, gss_buffer_t, OM_uint32 *, OM_uint32 *)"
 
+    if @LIBS.empty? and ! defined? Net::SSH::Kerberos::SSPI::Context
+      $stderr.puts "error: Failed to a find a supported GSS implementation on this platform (#{RUBY_PLATFORM})"
+    end
   end
 
-  if ! method_defined? :gss_acquire_cred and ! defined? Net::SSH::Kerberos::SSPI::Context
-    $stderr.puts "error: Failed to a find a supported GSS implementation on this platform (#{RUBY_PLATFORM})"
-  end
+  GSS_C_INITIATE = 1
 
-end; end; end; end; end
+  GSS_C_DELEG_FLAG      = 1
+  GSS_C_MUTUAL_FLAG     = 2
+  GSS_C_REPLAY_FLAG     = 4
+  GSS_C_SEQUENCE_FLAG   = 8
+  GSS_C_CONF_FLAG       = 16
+  GSS_C_INTEG_FLAG      = 32
+  GSS_C_ANON_FLAG       = 64
+  GSS_C_PROT_READY_FLAG = 128
+  GSS_C_TRANS_FLAG      = 256
+
+  GSS_C_NO_NAME         = nil
+  GSS_C_NO_BUFFER       = nil
+  GSS_C_NO_OID_SET      = nil
+  GSS_C_NO_CONTEXT      = nil
+  GSS_C_NO_CREDENTIAL   = nil
+
+end; end; end; end
--- test/gss_test.rb
+++ test/gss_test.rb
@@ -4,20 +4,49 @@ class GssTest < Test::Unit::TestCase
 
   include Net::SSH::Kerberos::GSS
 
-  def test_acquire_cred
+  def test_default_cred
+    principal = nil
+    req_mechs = nil
+    #mechs = API::GssOID.malloc
+    #mechs.length = 9
+    #mechs.elements = "\x2a\x86\x48\x86\xf7\x12\x01\x02\x02"
+    #req_mechs = API::GssOIDSet.malloc
+    #req_mechs.count = 1
+    #req_mechs.elements = mechs.to_ptr.ref
     minor_status = "\0" * 4
     creds = "\0" * 4
-    mechs = API::GssOID.malloc
-    mechs.length = 10
-    mechs.elements = "\x2a\x86\x48\x86\xf7\x12\x01\x02\x01\x01"
-    req_mechs = API::GssOIDSet.malloc
-    req_mechs.count = 1
-    req_mechs.elements = mechs.to_ptr.ref
     actual_mechs = "\0" * 4
     
-    result = API.gss_acquire_cred minor_status, nil, 10, req_mechs, API::GSS_C_INITIATE, creds, actual_mechs, nil
+    result = API.gss_acquire_cred minor_status, principal, 60, req_mechs, GSS_C_INITIATE, creds, actual_mechs, nil
     assert_equal 0, result, "gss_acquire_cred failed: 0x#{result.to_s(16)}"
+    assert_equal GSS_C_NO_CREDENTIAL, creds.unpack("P")[0], "Should acquire default credentials"
   end
+  
+  def test_init_sec_context
+    target_name = 'host@'+Socket.gethostbyname('localhost')[0]
+    buffer = API::GssBuffer.malloc
+    buffer.value = target_name
+    buffer.length = target_name.length
+    mechs = API::GssOID.malloc
+    mechs.length = 9
+    mechs.elements = "\x2a\x86\x48\x86\xf7\x12\x01\x02\x02"
+    minor_status = "\0" * 4
+    output = "\0" * 4
+    result = API.gss_import_name minor_status, buffer, mechs, output
+    assert_equal 0, result, "gss_import_name failed: 0x#{result.to_s(16)}"
+    assert_not_equal "\0"*4, output, "Should import the name"
+    output = API::GssOID.new(output.to_ptr)
+    target_name = output.value[0,output.length]
 
+    minor_status = "\0" * 4
+    ctx = "\0" * 4
+    actual_mech = "\0" * 4
+    buffer = "\0" * 4
+    
+    result = API.gss_init_sec_context minor_status, GSS_C_NO_CREDENTIAL,
+                ctx, target_name, GSS_C_NO_OID,
+                GSS_C_DELEG_FLAG | GSS_C_MUTUAL_FLAG | GSS_C_INTEG_FLAG, 60,
+                nil, GSS_C_NO_BUFFER, actual_mech, buffer, nil, nil
+  end
 end
 
