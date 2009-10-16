3baf424c97cfc1daf3b29b876f66e4af1e97526d
Joe Khoobyar <joe@ankhcraft.com>
2009-10-16 13:34:52 -0400

    more work on GSSAPI support

--- lib/net/ssh/kerberos/gss/api.rb
+++ lib/net/ssh/kerberos/gss/api.rb
@@ -7,6 +7,21 @@ module Net; module SSH; module Kerberos; module GSS;
 
     extend DL::Importable
 
+    def self.struct2(fields, &block)
+      t = struct fields
+      return t unless block_given?
+      t.instance_variable_set :@methods, Module.new(&block)
+      class << t
+        alias :new_struct :new
+        def new(ptr)
+          mem = new_struct(ptr)
+          mem.extend @methods
+          mem
+        end
+      end
+      t
+    end
+
     if RUBY_PLATFORM =~ /cygwin/
       dlload('cyggss-1.dll')
     else
@@ -19,33 +34,54 @@ module Net; module SSH; module Kerberos; module GSS;
     GssBuffer = struct [ "size_t length", "char *value" ]
     typealias 'gss_buffer_desc', 'GssBuffer'
     typealias 'gss_buffer_t', 'gss_buffer_desc *'
-    GssOID = struct [ "OM_uint32 length", "char *elements" ]
+    GssOID = struct2 [ "OM_uint32 length", "char *elements" ] do
+      def to_hex
+        s = elements.to_s
+        s.unpack("H2" * s.length).join ' '
+      end
+    end
     typealias 'gss_OID_desc', 'GssOID'
     typealias 'gss_OID', 'gss_OID_desc *'
     GssOIDSet = struct [ "size_t count", "gss_OID elements" ]
     typealias 'gss_OID_set_desc', 'GssOIDSet'
     typealias 'gss_OID_set', 'gss_OID_set_desc *'
 
-    #typealias 'gss_ctx_id_t', 'void *'
-    #typealias 'gss_cred_id_t', 'void *'
-    #typealias 'gss_name_t', 'void *'
-    #GssCtxId = struct [ "char *handle" ]
-    #GssCredId = struct [ "char *handle" ]
     typealias 'gss_ctx_id_t', 'void *'
     typealias 'gss_cred_id_t', 'void *'
     typealias 'gss_name_t', 'void *'
-    GssNameRef = struct [ "gss_name_t handle" ]
-    GssContextRef = struct [ "gss_ctx_id_t handle" ]
-    GssCredRef = struct [ "gss_cred_id_t handle" ]
-    GssMinorStatusRef = struct [ "OM_uint32 code" ]
-
     typealias 'gss_qop_t', 'OM_uint32'
     typealias 'gss_cred_usage_t', 'int'
 
+    GssNameRef = struct [ "gss_name_t handle" ]
+    GssContextRef = struct [ "gss_ctx_id_t handle" ]
+    GssCredRef = struct [ "gss_cred_id_t handle" ]
+    OM_uint32Ref = struct [ "OM_uint32 value" ]
+    GssCredUsageRef = struct [ "int value" ]
+
+    GssOIDRef = struct2 [ "GssOID *ptr" ] do
+      def oid
+        @oid = GssOID.new(ptr) if @oid.nil? or @oid.to_ptr != ptr
+        @oid
+      end
+    end
+
+    GssOIDSetRef = struct2 [ "GssOIDSet *ptr" ] do
+      def oidset
+        @oidset = GssOIDSet.new(ptr) if @oidset.nil? or @oidset.to_ptr != ptr
+        @oidset
+      end
+    end
+
     extern "OM_uint32 gss_acquire_cred (OM_uint32 *, gss_name_t, OM_uint32, gss_OID_set, gss_cred_usage_t, gss_cred_id_t *, gss_OID_set *, OM_uint32 *)"
+    extern "OM_uint32 gss_inquire_cred (OM_uint32 *, gss_cred_id_t, gss_name_t *, OM_uint32 *, gss_cred_usage_t *, gss_OID_set *)"
     extern "OM_uint32 gss_release_cred (OM_uint32 *, gss_cred_id_t *)"
     extern "OM_uint32 gss_import_name (OM_uint32 *, gss_buffer_t, gss_OID, gss_name_t *)"
+    extern "OM_uint32 gss_release_name (OM_uint32 *, gss_name_t *)"
+    extern "OM_uint32 gss_display_name (OM_uint32 *, gss_name_t, gss_buffer_t, gss_OID *)"
+    extern "OM_uint32 gss_release_buffer (OM_uint32 *, gss_buffer_t)"
+    extern "OM_uint32 gss_release_oid_set (OM_uint32 *, gss_OID_set *)"
     extern "OM_uint32 gss_init_sec_context (OM_uint32 *, gss_cred_id_t, gss_ctx_id_t *, gss_name_t, gss_OID, OM_uint32, OM_uint32, void *, gss_buffer_t, gss_OID *, gss_buffer_t, OM_uint32 *, OM_uint32 *)"
+    extern "OM_uint32 gss_delete_sec_context (OM_uint32 *, gss_ctx_id_t *, gss_buffer_t)"
 
     if @LIBS.empty? and ! defined? Net::SSH::Kerberos::SSPI::Context
       $stderr.puts "error: Failed to a find a supported GSS implementation on this platform (#{RUBY_PLATFORM})"
@@ -71,6 +107,13 @@ module Net; module SSH; module Kerberos; module GSS;
   GSS_C_NO_CONTEXT      = nil
   GSS_C_NO_CREDENTIAL   = nil
   GSS_C_NO_CHANNEL_BINDINGS = nil
+
+  GSS_S_COMPLETE        = 0
+  GSS_S_CONTINUE_NEEDED = 1
+  GSS_S_DUPLICATE_TOKEN = 2
+  GSS_S_OLD_TOKEN       = 4
+  GSS_S_UNSEQ_TOKEN     = 8
+  GSS_S_GAP_TOKEN       = 16
 
   #--
   # GSSAPI / Kerberos 5  OID(s)
--- test/gss_test.rb
+++ test/gss_test.rb
@@ -4,39 +4,94 @@ class GssTest < Test::Unit::TestCase
 
   include Net::SSH::Kerberos::GSS
 
-  def test_default_cred
-    minor_status = "\0" * 4
-    creds = "\0" * 4
-    result = API.gss_acquire_cred minor_status, nil, 60, nil, GSS_C_INITIATE, creds, nil, nil
-    assert_equal result, 0, "gss_acquire_cred failed: 0x#{result.to_s(16)}"
-    assert_equal creds.unpack("P")[0], GSS_C_NO_CREDENTIAL, "Should acquire default credentials"
+  def test_acquire_cred
+    creds = API::GssCredRef.malloc
+    result = call_and_assert :gss_acquire_cred, nil, 60, nil, GSS_C_INITIATE, creds, nil, nil
+    assert_not_equal 0, creds.handle.to_i, "Should acquire default credentials"
+    begin
+      name = API::GssNameRef.malloc
+      lifetime = API::OM_uint32Ref.malloc
+      usage = API::GssCredUsageRef.malloc
+      oids = API::GssOIDSetRef.malloc
+      result = call_and_assert :gss_inquire_cred, creds.handle, name, nil, usage, oids
+      assert_not_equal 0, name.handle.to_i, "Should provide the internal name"
+      assert_not_equal 0, oids.oidset.count, "Should provide the supported oids"
+      begin
+        buffer = API::GssBuffer.malloc
+        oid = API::GssOIDRef.malloc
+        assert_equal GSS_C_INITIATE, usage.value, "Usage should specify GSS_C_INITIATE"
+        result = call_and_assert :gss_display_name, name.handle, buffer, oid
+        assert_not_equal 0, buffer.value.to_i, "Should provide the display name"
+        begin
+          assert_not_equal 0, oid.ptr.to_i, "Should provide the supported oid"
+          #$stderr.puts "credentials: #{buffer.value} (OID: #{oid.oid.length}, #{oid.oid.to_hex})"
+        ensure
+          result = API.gss_release_buffer API::OM_uint32Ref.malloc, buffer
+        end
+      ensure
+        minor_status = API::OM_uint32Ref.malloc
+        API.gss_release_name minor_status, name
+        API.gss_release_oid_set minor_status, oids
+        assert_equal 0, name.handle.to_i, "Should release the internal name"
+        assert_equal 0, oids.ptr.to_i, "Should release the supported oids"
+      end
+    ensure
+      minor_status = API::OM_uint32Ref.malloc
+      API.gss_release_cred minor_status, creds
+    end
   end
   
   def test_init_sec_context
-    target_name = 'host/usbillingstg.corp.tnsi.com' #'host@'+Socket.gethostbyname('localhost')[0]
+    target_name = 'host@'+Socket.gethostbyname(`hostname || echo "localhost"`.strip)[0]
     buffer = API::GssBuffer.malloc
     buffer.value = target_name
     buffer.length = target_name.length
-    mechs = API::GssOID.malloc
-    mechs.elements = GSS_C_NT_HOSTBASED_SERVICE
-    mechs.length = GSS_C_NT_HOSTBASED_SERVICE.length
-    minor_status = API::GssMinorStatusRef.malloc
+    mech = API::GssOID.malloc
+    mech.elements = GSS_C_NT_HOSTBASED_SERVICE
+    mech.length = GSS_C_NT_HOSTBASED_SERVICE.length
     target_name = API::GssNameRef.malloc
-    result = API.gss_import_name minor_status, buffer, mechs, target_name
-    assert_equal result, 0, "gss_import_name failed: 0x#{result.to_s(16)}"
-    assert_equal minor_status.code, 0, "gss_import_name failed: minor status 0x#{minor_status.code.to_s(16)}"
+    result = call_and_assert :gss_import_name, buffer, mech, target_name
     assert_not_equal target_name.handle, GSS_C_NO_NAME, "Should import the name"
 
-    actual_mech = "\0" * 4
+    buffer = API::GssBuffer.malloc
+    result = call_and_assert :gss_display_name, target_name.handle, buffer, nil
+    assert_not_equal 0, buffer.value.to_i, "Should provide the display name"
+    #$stderr.puts "target: #{buffer.value} (OID: #{mech.length}, #{mech.to_hex})"
+    call_and_assert :gss_release_buffer, buffer
+
+    mech.elements = GSS_KRB5_MECH
+    mech.length = GSS_KRB5_MECH.length
+    actual_mech = API::GssOIDRef.malloc
+    context = API::GssContextRef.malloc
+    context.handle = GSS_C_NO_CONTEXT
     buffer.value = nil
     buffer.length = 0
-    result = API.gss_init_sec_context minor_status, GSS_C_NO_CREDENTIAL,
-                GSS_C_NO_CONTEXT, target_name.handle, GSS_C_NO_OID,
-                GSS_C_DELEG_FLAG | GSS_C_MUTUAL_FLAG | GSS_C_INTEG_FLAG, 60,
-                GSS_C_NO_CHANNEL_BINDINGS, GSS_C_NO_BUFFER, actual_mech.to_ptr.ref, buffer, nil, nil
-    assert_equal result, 0, "gss_init_sec_context failed: 0x#{result.to_s(16)}"
-    assert_equal minor_status.code, 0, "gss_init_sec_context failed: minor status 0x#{minor_status.code.to_s(16)}"
+    result = call_and_assert :gss_init_sec_context, GSS_C_NO_CREDENTIAL, context, target_name.handle, mech,
+                              GSS_C_DELEG_FLAG | GSS_C_MUTUAL_FLAG | GSS_C_INTEG_FLAG, 60,
+                              GSS_C_NO_CHANNEL_BINDINGS, GSS_C_NO_BUFFER, actual_mech, buffer, nil, nil
+    assert_not_equal 0, context.handle.to_i, "Should initialize the security context"
+    begin
+      assert_equal GSS_S_CONTINUE_NEEDED, result, "Should need continued initialization of the security context"
+      assert buffer.length > 0, "Should output a token to send to the server"
+      #$stderr.puts "context: (#{buffer.length}) (OID: #{actual_mech.oid.length}, #{actual_mech.oid.to_hex})"
+      call_and_assert :gss_release_buffer, buffer
+    ensure
+      minor_status = API::OM_uint32Ref.malloc
+      API.gss_delete_sec_context minor_status, context, nil
+      if buffer.value.nil?
+        assert_equal 0, context.handle.to_i, "Should delete the security context"
+      end
+    end
+  end
 
+private
+  
+  def call_and_assert(sym, *args)
+    minor_status = API::OM_uint32Ref.malloc
+    result = API.send sym, minor_status, *args
+    assert_equal 0, (result & 0xffff0000), "#{sym} failed: 0x#{result.to_s(16)}"
+    assert_equal 0, minor_status.value, "#{sym} failed: minor status 0x#{minor_status.value.to_s(16)}"
+    result
   end
 end
 
