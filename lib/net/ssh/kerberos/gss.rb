0b79b58dd570c35cdb68bd18431d2f03e41a941b
Joe Khoobyar <joe@ankhcraft.com>
2009-10-18 21:55:44 -0400

    refactoring and simplifying kerberos drivers

--- lib/net/ssh/kerberos/gss.rb
+++
@@ -1,9 +0,0 @@
-if ! defined? Net::SSH::Kerberos::SSPI::Context
-  $stderr.puts "warning: Kerberos support using GSSAPI is not fully tested."
-end
-
-require 'net/ssh/kerberos/gss/api'
-require 'net/ssh/kerberos/gss/context'
-
-module Net; module SSH; module Kerberos; module GSS
-end; end; end; end
--- lib/net/ssh/kerberos/sspi.rb
+++
@@ -1,5 +0,0 @@
-require 'net/ssh/kerberos/sspi/api'
-require 'net/ssh/kerberos/sspi/context'
-
-module Net; module SSH; module Kerberos; module SSPI
-end; end; end; end
