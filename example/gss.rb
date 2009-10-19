dd39924a6e8b5d937f11346fc01fbd8acf7f455b
Joe Khoobyar <joe@ankhcraft.com>
2009-10-18 22:18:09 -0400

    added example files

---
+++ example/gss.rb
@@ -0,0 +1,89 @@
+$: << File.join(File.dirname(__FILE__), '..', '..', 'lib')
+require 'socket'
+require 'rubygems'
+gem 'net-ssh'
+require 'net/ssh'
+require 'net/ssh/errors'
+require 'net/ssh/kerberos/constants'
+require 'net/ssh/kerberos/context'
+require 'net/ssh/kerberos/drivers'
+require 'net/ssh/kerberos/drivers/gss'
+
+include Net::SSH::Kerberos::Drivers::GSS
+include Net::SSH::Kerberos::Constants
+
+result = API.gss_acquire_cred nil, 60, nil, GSS_C_INITIATE, nil, nil, 0
+if result.ok?
+  creds = API._args_[4]
+  $stderr.puts "gss_acquire_cred: (#{result}) => #{creds.to_i}"
+  begin
+    result = API.gss_inquire_cred creds, nil, 0, 0, nil
+    if result.ok?
+      name, oids = API._args_[1], API._args_[4]
+      $stderr.puts "gss_inquire_cred: (#{result}) #{oids.inspect}"
+      begin
+        result = API.gss_display_name name, buffer=API::GssBuffer.malloc, nil
+        if result.ok?
+          oid = API._args_[2]
+          $stderr.puts "gss_display_name: (#{result}) #{buffer} #{oid.inspect}"
+          result = API.gss_release_buffer buffer
+          $stderr.puts "gss_release_buffer: (#{result})"
+        else
+          $stderr.puts "gss_display_name failed : (#{result})"
+        end
+      ensure
+        result = API.gss_release_oid_set oids
+        $stderr.puts "gss_release_oid_set: (#{result})"
+        result = API.gss_release_name name
+        $stderr.puts "gss_release_name: (#{result})"
+      end
+    else
+      $stderr.puts "gss_inquire_cred failed: (#{result})"
+    end
+
+
+    target_name = 'host@'+Socket.gethostbyname(`hostname || echo "localhost"`.strip)[0]
+    buffer = API::GssBuffer.malloc
+    buffer.value = target_name
+    buffer.length = target_name.length
+    API.gss_import_name buffer, GSS_C_NT_HOSTBASED_SERVICE, nil
+    if result.ok?
+      target = API._args_[2]
+      $stderr.puts "gss_import_name: (#{result}) #{target.to_i}"
+      begin
+        result = API.gss_display_name target, buffer, nil
+        if result.ok?
+          oid = API._args_[2]
+          $stderr.puts "gss_display_name: (#{result}) #{buffer} #{oid.inspect}"
+          result = API.gss_release_buffer buffer
+          $stderr.puts "gss_release_buffer: (#{result})"
+        else
+          $stderr.puts "gss_display_name failed : (#{result})"
+        end
+        result = API.gss_init_sec_context creds, GSS_C_NO_CONTEXT, target, GSS_C_KRB5,
+                                          GSS_C_DELEG_FLAG | GSS_C_MUTUAL_FLAG | GSS_C_INTEG_FLAG, 60,
+                                          GSS_C_NO_CHANNEL_BINDINGS, GSS_C_NO_BUFFER, nil, buffer, 0, 0
+        if result.ok?
+          context, actual_mech = API._args_[1], API._args_[8]
+          $stderr.puts "gss_init_sec_context: (#{result}) token.length=#{buffer.length}, #{actual_mech.inspect}"
+          result = API.gss_release_buffer buffer
+          $stderr.puts "gss_release_buffer: (#{result})"
+          result = API.gss_delete_sec_context context, nil
+          $stderr.puts "gss_delete_sec_context: (#{result})"
+        else
+          $stderr.puts "gss_init_sec_context failed : (#{result})"
+        end
+      ensure
+        result = API.gss_release_name target
+        $stderr.puts "gss_release_name: (#{result})"
+      end
+    else
+      $stderr.puts "gss_import_name failed: (#{result})"
+    end
+  ensure
+    result = API.gss_release_cred creds
+    $stderr.puts "gss_release_cred: (#{result})"
+  end
+else
+  $stderr.puts "gss_acquire_cred failed: (#{result})"
+end
---
+++ example/sspi.rb
@@ -0,0 +1,71 @@
+#$DEBUG = 1
+
+$: << File.join(File.dirname(__FILE__), '..', '..', 'lib')
+require 'socket'
+require 'rubygems'
+gem 'net-ssh'
+require 'net/ssh'
+require 'net/ssh/errors'
+require 'net/ssh/kerberos/constants'
+require 'net/ssh/kerberos/context'
+require 'net/ssh/kerberos/drivers'
+require 'net/ssh/kerberos/drivers/sspi'
+
+include Net::SSH::Kerberos::Drivers::SSPI
+include Net::SSH::Kerberos::Constants
+
+result = API.querySecurityPackageInfo "Kerberos", nil
+if result.ok?
+  pkg_info = API._args_[1]
+  $stderr.puts "querySecurityPackageInfo: (#{result}) #{pkg_info.comment} (max_token=#{pkg_info.max_token})"
+  @max_token = pkg_info.max_token
+  result = API.freeContextBuffer pkg_info
+  $stderr.puts "freeContextBuffer: (#{result})"
+else
+  $stderr.puts "querySecurityPackageInfo: (#{result})"
+end
+
+result = API.acquireCredentialsHandle nil, "Kerberos", SECPKG_CRED_OUTBOUND, nil, nil, nil, nil,
+                                      creds=API::SecHandle.malloc, ts=API::TimeStamp.malloc
+if result.ok?
+  $stderr.puts "acquireCredentialsHandle: (#{result})"
+  begin
+    result = API.queryCredentialsAttributes creds, SECPKG_ATTR_NAMES, nil
+    if result.ok?
+      names = API._args_[2]
+      $stderr.puts "queryCredentialsAttributes: (#{result}) #{names.to_s}"
+      result = API.freeContextBuffer names
+      $stderr.puts "freeContextBuffer: (#{result})"
+
+      token = API::SecBuffer.malloc
+      token.type = SECBUFFER_TOKEN
+      token.data = "\0" * @max_token
+      token.length = @max_token
+      output = API::SecBufferDesc.malloc
+      output.version = 0
+      output.count = 1
+      output.buffers = token.to_ptr
+      result = API.initializeSecurityContext creds, nil, 'host/'+Socket.gethostbyname('localhost')[0], 
+                                             ISC_REQ_DELEGATE | ISC_REQ_MUTUAL_AUTH | ISC_REQ_INTEGRITY, 0, SECURITY_NATIVE_DREP,
+                                             nil, 0, ctx=API::SecHandle.malloc, output, 0, ts=API::TimeStamp.malloc
+      if result.ok?
+        $stderr.puts "initializeSecurityContext: (#{result}) ctx=#{! ctx.nil?} token.length=#{output.buffer(0).length}"
+        result = API.freeContextBuffer token.data
+        $stderr.puts "freeContextBuffer: (#{result})"
+        result = API.deleteSecurityContext ctx
+        $stderr.puts "deleteSecurityContext: (#{result})"
+      else
+        $stderr.puts "initializeSecurityContext: (#{result})"
+      end
+    else
+      $stderr.puts "queryCredentialsAttributes: (#{result})"
+    end
+  ensure
+    result = API.freeCredentialsHandle creds
+    $stderr.puts "freeCredentialsHandle : (#{result})"
+  end
+else
+  $stderr.puts "acquireCredentialsHandle: (#{result})"
+end
+
+
