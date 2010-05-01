a06d84bcff27e7e445a989b64219e131e5a4c83a
Joe Khoobyar <joe@ankhcraft.com>
2010-05-01 01:41:38 -0400

    fix mingw32 detection to try SSPI

--- lib/net/ssh/kerberos/drivers.rb
+++ lib/net/ssh/kerberos/drivers.rb
@@ -46,7 +46,7 @@ module Net; module SSH; module Kerberos;
 		@@available = []
     def self.available; @@available end
 
-		if RUBY_PLATFORM.include?('win') && ! RUBY_PLATFORM.include?('dar'); then
+		if %w(win dar mingw).any?{|p| RUBY_PLATFORM.include?(p)}; then
 		  begin require 'net/ssh/kerberos/drivers/sspi'; available << 'SSPI'
 		  rescue => e
 		    raise e unless RuntimeError === e and e.message =~ /^LoadLibrary: ([^\s]+)/
