68c50fb71dab79273cb1e54d45f85222fbf9b963
Chris Beer <cabeer@stanford.edu>
2013-04-01 13:51:21 -0700

    remove unused tests (now that we're delegating GSS work to the GSSAPI gem

--- test/integration_test.rb
+++
@@ -1,5 +0,0 @@
-require 'rubygems'
-require File.expand_path(File.join(File.dirname(__FILE__), 'test_helper.rb'))
-
-%w(net/ssh net/ssh/kerberos).each { |lib| require lib }
-puts Net::SSH.start(ARGV.shift, ENV['USER'], :auth_methods => ["gssapi-with-mic", "publickey"]).exec! "whoami"
--- test/net_ssh_kerberos_test.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), 'test_helper.rb')
-
-class NetSshKerberosTest < Test::Unit::TestCase
-  def test_net_ssh_integration
-    flunk "write some unit tests for Net::SSH integration"
-  end
-end
